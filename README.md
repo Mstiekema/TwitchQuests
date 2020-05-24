9dbaf81e222521a4373c20d1869c1bbf5109564d
Mstiekema <merstiekema@gmail.com>
2020-05-24 21:17:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TwitchQuests 
+TwitchQuests aims to increase engagement in Twitch chat. Users complete quests to earn XP and currency which can be spent in chat or on the website.
+
+## TwitchQuests UI
+[GitHub Repo here](https://github.com/Mstiekema/TwitchQuests.UI)
+
+## Run application
+`cargo run`
+
+## Run build
+`cargo build --release`
---
+++ src/main.rs
@@ -0,0 +1,85 @@
+use chrono::prelude::*;
+use futures::prelude::*;
+use irc::client::prelude::*;
+use std::collections::LinkedList;
+
+#[tokio::main]
+async fn main() -> irc::error::Result<()> {
+  // Handle connection to chat
+  println!("[DEBUG] Connecting to Twitch Chat...");
+  let mut client = Client::new("config.toml").await?;
+  client.identify()?;
+  let mut stream = client.stream()?;
+  let sender = client.sender();
+  println!("[DEBUG] Connected to Twitch Chat");
+
+  // Handle all messages
+  while let Some(message) = stream.next().await.transpose()? {
+    match message.command {
+      Command::PRIVMSG(ref target, ref txt) => {
+        let msg_obj = handle_messages(message.clone(), target, txt);
+
+        if msg_obj.command {
+          handle_command(msg_obj.clone(), sender.clone());
+        }
+
+        println!("[{}] ({}) {}: {}", msg_obj.date.format("%d/%m/%Y %T"), msg_obj.channel, msg_obj.user, msg_obj.text);
+      }
+      _ => ()
+    }
+  }
+
+  Ok(())
+}
+
+// Handle commands calls
+fn handle_command(msg: MessageObject, sender: irc::client::Sender) {
+  match msg.command_txt.as_ref() {
+    "!test" => sender.send_privmsg(msg.channel, "Testing 123 MrDestructoid").unwrap(),
+    _ => sender.send_privmsg(msg.channel, "A command was used :)").unwrap()
+  }
+}
+
+// Assign the message to the custom message object
+fn handle_messages(msg: irc::client::prelude::Message, target: &String, txt: &String) -> MessageObject {
+  let mut message = MessageObject {
+    text: txt.to_string(),
+    user: msg.source_nickname().unwrap().replace("#", "").to_string(),
+    channel: target.to_string(),
+    ..Default::default()
+  };
+
+  if txt.contains("!") {
+    let mut message_array = txt.split_whitespace();
+    message.command = true;
+    message.command_txt = message_array.next().unwrap().to_string();
+    message.command_args = message_array.map(|word| word.to_string()).collect::<LinkedList<String>>();
+  }
+  
+  return message;
+}
+
+#[derive(Clone)]
+struct MessageObject {
+  text: String,
+  channel: String,
+  user: String,
+  date: DateTime<Local>,
+  command: bool,
+  command_txt: String,
+  command_args: LinkedList<String>
+}
+
+impl Default for MessageObject {
+  fn default() -> MessageObject {
+    MessageObject {
+      text: String::from(""),
+      user: String::from(""),
+      channel: String::from(""),
+      date: Local::now(),
+      command: false,
+      command_txt: String::from(""),
+      command_args: LinkedList::new()
+    }
+  }
+}
